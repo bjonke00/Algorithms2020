cd9544a96d18a20efc7024e900fa501246f54d1f
probe <re2rar@gmail.com>
2020-05-31 02:17:59 +0900

    removed uniqueNumbers

--- AlgorithmPractice/test/com/algorithmpractice/other/UniqueOccurrencesTest.java
+++ AlgorithmPractice/test/com/algorithmpractice/other/UniqueOccurrencesTest.java
@@ -9,21 +9,21 @@ import org.junit.Test;
 
 public class UniqueOccurrencesTest {
 
-	private UniqueOccurrences uniqueOccurrences = new UniqueOccurrences();
-	
-	@Test
-	public void test1() {
-		assertTrue(uniqueOccurrences.uniqueOccurrences(Arrays.asList(new Integer[]{1,2,2,1,1,3})));
-	}
-	
-	@Test
-	public void test2() {
-		assertFalse(uniqueOccurrences.uniqueOccurrences(Arrays.asList(new Integer[]{1,2})));
-	}
-	
-	@Test
-	public void test3() {
-		assertTrue(uniqueOccurrences.uniqueOccurrences(Arrays.asList(new Integer[]{-3,0,1,-3,1,1,1,-3,10,0})));
-	}
+    private final UniqueOccurrences uniqueOccurrences = new UniqueOccurrences();
+
+    @Test
+    public void test1() {
+        assertTrue(uniqueOccurrences.uniqueOccurrences(Arrays.asList(1, 2, 2, 1, 1, 3)));
+    }
+
+    @Test
+    public void test2() {
+        assertFalse(uniqueOccurrences.uniqueOccurrences(Arrays.asList(1, 2)));
+    }
+
+    @Test
+    public void test3() {
+        assertTrue(uniqueOccurrences.uniqueOccurrences(Arrays.asList(-3, 0, 1, -3, 1, 1, 1, -3, 10, 0)));
+    }
 
 }
