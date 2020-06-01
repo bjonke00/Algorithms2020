7b7cfe45d6ea21b0d8815c83d7688d08536e86f4
bjonke00 <bjonke00@gmail.com>
2020-05-31 22:08:14 -0400

    improve FirstUniqueCharacterInString

--- AlgorithmPractice/src/com/algorithmpractice/leetcode/FirstUniqueCharacterInString.java
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/FirstUniqueCharacterInString.java
@@ -6,7 +6,25 @@ import java.util.Map;
 public class FirstUniqueCharacterInString {
 //	Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
 
-	public int findFirstUniqueCharacter(String str){
+	//O(n) time and O(1) space
+	public int findFirstUniqueCharacter(String str) {
+
+		int alphabet[] = new int[26];
+
+		for (int i = 0; i < str.length(); i++) {
+			alphabet[str.charAt(i)-'a']++;
+		}
+
+		for (int i = 0; i < str.length(); i++) {
+			if (alphabet[str.charAt(i)-'a'] == 1)
+				return i;
+		}
+
+		return -1;
+	}
+	
+	//O(n) time and O(n) space
+	public int findFirstUniqueCharacter1(String str){
 		
 		Map<Character, Integer> uniqueCharMap = new HashMap<>();
 		
