3f9623fc50a389c9564b3e82eaf8dc10255692cf
bjonke00 <bjonke00@gmail.com>
2020-05-31 21:38:20 -0400

    update ValidAnagram

--- AlgorithmPractice/src/com/algorithmpractice/leetcode/ValidAnagram.java
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/ValidAnagram.java
@@ -6,31 +6,52 @@ import java.util.Map;
 public class ValidAnagram {
 //	Given two strings s and t , write a function to determine if t is an anagram of s.
 
+	//O(n) time and O(1) space
 	public boolean isValidAnagram(String str1, String str2) {
 		
-		Map<Character, Integer> charMap1 = new HashMap<>();
-		Map<Character, Integer> charMap2 = new HashMap<>();
+		int alphabet[] = new int[26];
+		
+		if(str1.length()!= str2.length())
+			return false;
 		
 		for(int i=0; i<str1.length(); i++) {
-			charMap1.put(str1.charAt(i), charMap1.getOrDefault(str1.charAt(i),0)+1);
+			alphabet[str1.charAt(i)-'a']++;
 		}
 		
 		for(int i=0; i<str2.length(); i++) {
-			charMap2.put(str2.charAt(i), charMap2.getOrDefault(str2.charAt(i),0)+1);
+			alphabet[str2.charAt(i)-'a']--;
 		}
 		
-		for(Character i : charMap1.keySet()) {
-			if(charMap1.get(i) != charMap2.get(i)) {
+		for(int i : alphabet) {
+			if(i!=0)
+				return false;
+		}
+				
+		return true;
+	}
+	
+	//O(n) time and O(n) space
+	public boolean isValidAnagram1(String str1, String str2) {
+		
+		Map<Character, Integer> charMap = new HashMap<>();
+		
+		if(str1.length()!= str2.length())
+			return false;
+		
+		for(int i=0; i<str1.length(); i++) {
+			charMap.put(str1.charAt(i), charMap.getOrDefault(str1.charAt(i),0)+1);
+		}
+		
+		for(int i=0; i<str2.length(); i++) {
+			charMap.put(str2.charAt(i), charMap.getOrDefault(str2.charAt(i),0)-1);
+		}
+		
+		for(Character i : charMap.keySet()) {
+			if(charMap.get(i) != 0) {
 				return false;
 			}
 		}
-		
-		for(Character i : charMap2.keySet()) {
-			if(charMap1.get(i) != charMap2.get(i)) {
-				return false;
-			}
-		}
-		
+				
 		return true;
 	}
 	
