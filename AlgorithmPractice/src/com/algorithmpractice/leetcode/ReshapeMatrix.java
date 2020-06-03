3040579aa7fff89632aa442afdb5bfd82e3619da
bjonke00 <bjonke00@gmail.com>
2020-06-03 01:07:46 -0400

    add ReshapeMatrix

---
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/ReshapeMatrix.java
@@ -0,0 +1,29 @@
+package com.algorithmpractice.leetcode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ReshapeMatrix {
+
+	public int[][] matrixReshape(int[][] nums, int r, int c) {
+		if (nums.length * nums[0].length != r * c)
+			return nums;
+
+		Queue<Integer> queue = new LinkedList();
+		for (int i = 0; i < nums.length; i++) {
+			for (int j = 0; j < nums[i].length; j++) {
+				queue.add(nums[i][j]);
+			}
+		}
+
+		int[][] reshaped = new int[r][c];
+		for (int i = 0; i < r; i++) {
+			for (int j = 0; j < c; j++) {
+				reshaped[i][j] = queue.poll();
+			}
+		}
+
+		return reshaped;
+	}
+
+}
