fd761bde688f92fb035b890209780e831d5d4c0a
bjonke00 <bjonke00@gmail.com>
2020-06-04 22:45:16 -0400

    update space complexity for findDuplicate

--- AlgorithmPractice/src/com/algorithmpractice/other/GreaterNumbersFinder.java
+++ AlgorithmPractice/src/com/algorithmpractice/other/GreaterNumbersFinder.java
@@ -1,7 +1,12 @@
 package com.algorithmpractice.other;
 
 public class GreaterNumbersFinder {
-
+    // No. 2 **sort**
+//  Write a function called findGreaterNumbers which accepts an array
+//  and returns the number of times a number is followed by a larger number.
+//  Note that the numbers don't need to be next to each other in the array.
+//  Any pair where the second number comes later in the array and is also
+//  the larger number should count.
     public int[] findGreaterNumbers(){
 
         return new int[0];
--- AlgorithmPractice/test/com/algorithmpractice/other/GreaterNumbersFinderTest.java
+++ AlgorithmPractice/test/com/algorithmpractice/other/GreaterNumbersFinderTest.java
@@ -5,6 +5,10 @@ import org.junit.Before;
 import static org.junit.Assert.*;
 
 public class GreaterNumbersFinderTest {
+//            console.log(findGreaterNumbers([1,2,3])) // 3 (2 > 1, 3 > 2, and 3 > 1)
+//            console.log(findGreaterNumbers([6,1,2,7])) // 4
+//            console.log(findGreaterNumbers([5,4,3,2,1])) // 0
+//            console.log(findGreaterNumbers([])) // 0
 
 
 }
