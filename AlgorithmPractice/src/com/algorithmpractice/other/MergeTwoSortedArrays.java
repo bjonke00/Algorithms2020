4b3f1b1ea79c20eb5867c76313eb01c49483def1
bjonke00 <bjonke00@gmail.com>
2020-06-11 01:27:41 -0400

    add MergeTwoSortedArrays

---
+++ AlgorithmPractice/src/com/algorithmpractice/other/MergeTwoSortedArrays.java
@@ -0,0 +1,37 @@
+package com.algorithmpractice.other;
+
+public class MergeTwoSortedArrays {
+
+    public int[] mergeTwoSortedArrays(int[] input1, int[] input2){
+        int[] output = new int[input1.length + input2.length];
+
+        int i = 0;
+        int j = 0;
+        int count = 0;
+
+        while(i<input1.length && j<input2.length){
+            if(input1[i]<input2[j]){
+                output[count] = input1[i];
+                i++;
+            }else if(input1[i]>input2[j]){
+                output[count] = input2[j];
+                j++;
+            }
+            count++;
+        }
+
+        while(i<input1.length){
+          output[count] = input1[i];
+          i++;
+          count++;
+        }
+
+        while(j<input2.length){
+            output[count] = input2[j];
+            j++;
+            count++;
+        }
+
+        return output;
+    }
+}
---
+++ AlgorithmPractice/test/com/algorithmpractice/other/MergeTwoSortedArraysTest.java
@@ -0,0 +1,34 @@
+package com.algorithmpractice.other;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+public class MergeTwoSortedArraysTest {
+
+    private MergeTwoSortedArrays mergeTwoSortedArrays;
+
+    @Before
+    public void setup(){
+        mergeTwoSortedArrays = new MergeTwoSortedArrays();
+    }
+
+    @Test
+    public void testMergeTwoSortedArrays(){
+        int[] input1 = new int[]{3,4,7,8};
+        int[] input2 = new int[]{1,5,6,9};
+        int[] output = new int[]{1,3,4,5,6,7,8,9};
+        assertThat(mergeTwoSortedArrays.mergeTwoSortedArrays(input1,input2), is(equalTo(output)));
+    }
+
+    @Test
+    public void testMergeTwoSortedArrays1(){
+        int[] input1 = new int[]{25};
+        int[] input2 = new int[]{0,50,55,75,80,100};
+        int[] output = new int[]{0,25,50,55,75,80,100};
+        assertThat(mergeTwoSortedArrays.mergeTwoSortedArrays(input1,input2), is(equalTo(output)));
+    }
+
+}
