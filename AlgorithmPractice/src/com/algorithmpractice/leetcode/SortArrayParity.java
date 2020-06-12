4ec649d3973ba9d3cca0f4a6398023f92e0adac7
bjonke00 <bjonke00@gmail.com>
2020-06-11 23:20:15 -0400

    add SortArrayParityTest

---
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/SortArrayParity.java
@@ -0,0 +1,10 @@
+package com.algorithmpractice.leetcode;
+
+public class SortArrayParity {
+//    Given an array A of non-negative integers, return an array consisting of all the even elements of A, followed by all the odd elements of A.
+//    You may return any answer array that satisfies this condition.
+
+    public int[] sortArrayByParity(int[] A) {
+
+    }
+}
---
+++ AlgorithmPractice/test/com/algorithmpractice/leetcode/SortArrayParityTest.java
@@ -0,0 +1,26 @@
+package com.algorithmpractice.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.MatcherAssert.*
+
+public class SortArrayParityTest {
+
+    private SortArrayParity sortArrayParity;
+
+    @Before
+    public void setup(){
+        sortArrayParity = new SortArrayParity();
+    }
+
+    @Test
+    public void test(){
+        int[] input = new int[]{3,1,2,4};
+        int[] output = new int[]{2,4,3,1};
+        assertThat(sortArrayParity.sortArrayByParity(input), is(equalTo(output)));
+    }
+
+    //    Output: []
+//    The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+}
