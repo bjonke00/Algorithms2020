41a21a3d4e09881a4f91210be8cc5935b7bc1144
bjonke00 <bjonke00@gmail.com>
2020-06-12 21:39:43 -0400

    add SortedArrayParityIITest

---
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/SortedArrayParityII.java
@@ -0,0 +1,14 @@
+package com.algorithmpractice.leetcode;
+
+public class SortedArrayParityII {
+
+//    Given an array A of non-negative integers, half of the integers in A are odd, and half of the integers are even.
+//    Sort the array so that whenever A[i] is odd, i is odd; and whenever A[i] is even, i is even.
+//    You may return any answer array that satisfies this condition.
+
+    public int[] sortArrayByParityII(int[] A) {
+        int[] output = new int[A.length];
+
+        return output;
+    }
+}
---
+++ AlgorithmPractice/test/com/algorithmpractice/leetcode/SortedArrayParityIITest.java
@@ -0,0 +1,28 @@
+package com.algorithmpractice.leetcode;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class SortedArrayParityIITest{
+
+    private SortedArrayParityII sortedArrayParityII;
+
+    @Before
+    public void setup() {
+        sortedArrayParityII = new SortedArrayParityII();
+    }
+
+    @Test
+    public void test(){
+        int[] input = new int[]{4,2,5,7};
+        int[] output = new int[]{4,5,2,7};
+        assertThat(sortedArrayParityII.sortArrayByParityII(input), is(equalTo(output));
+    }
+
+//      [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+
+}
