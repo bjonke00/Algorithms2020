a24efdbff977043ce9bf5aaaa81daf21b3b0b649
bjonke00 <bjonke00@gmail.com>
2020-06-15 00:38:58 -0400

    updated solution

--- AlgorithmPractice/src/com/algorithmpractice/leetcode/FlipAndInvertImage.java
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/FlipAndInvertImage.java
@@ -9,7 +9,20 @@ public class FlipAndInvertImage {
 //    To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
 //    To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
 
+    //O(n) time and O(1) space
+    class Solution {
+        public int[][] flipAndInvertImage(int[][] A) {
+            int C = A[0].length;
+            for (int[] row: A)
+                for (int i = 0; i < (C + 1) / 2; ++i) {
+                    int tmp = row[i] ^ 1;
+                    row[i] = row[C - 1 - i] ^ 1;
+                    row[C - 1 - i] = tmp;
+                }
 
+            return A;
+        }
+    }
 
     //O(n*m)*2 time and space (n*m)
     public int[][] flipAndInvertImage(int[][] A) {
