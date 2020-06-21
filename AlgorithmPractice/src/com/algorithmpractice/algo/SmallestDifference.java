09724523f2526c53993602275a97da8fb6d586a5
bjonke00 <bjonke00@gmail.com>
2020-06-21 19:41:09 -0400

    commit SmallestDifference

---
+++ AlgorithmPractice/src/com/algorithmpractice/algo/SmallestDifference.java
@@ -0,0 +1,42 @@
+package com.algorithmpractice.algo;
+
+import java.util.Arrays;
+
+/*
+    Write a function that takes two non-empty arrays of integers, finds the pair of numbers (one from ea. array)
+    whose absolute difference is closest to zero, and returns an array containing the pair.
+    Assume there is only one pair with the smallest difference.
+ */
+public class SmallestDifference {
+    // O(nlogn) time & O(1) space
+    public static int[] smallestDifference(int[] arrayOne, int[] arrayTwo) {
+        //sort arrays & use 2 pointers
+        Arrays.sort(arrayOne);
+        Arrays.sort(arrayTwo);
+        int p1 = 0;
+        int p2 = 0;
+        int smallest = Integer.MAX_VALUE;
+        int current;
+        int[] output = new int[2];
+        while (p1 < arrayOne.length && p2 < arrayTwo.length) {
+            int first = arrayOne[p1];
+            int second = arrayTwo[p2];
+
+            if (first == second) {
+                return new int[]{first, second};
+            } else if (first < second) {     //increment the pointer with the smaller number
+                current = second - first;
+                p1++;
+            } else {
+                current = first - second;
+                p2++;
+            }
+            if (smallest > current) {     //keep track of the two numbers that have the smallest difference
+                smallest = current;
+                output = new int[]{first, second};
+            }
+        }
+        return output;
+    }
+
+}
---
+++ AlgorithmPractice/test/com/algorithmpractice/algo/SmallestDifferenceTest.java
@@ -0,0 +1,39 @@
+package com.algorithmpractice.algo;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+
+public class SmallestDifferenceTest {
+
+    private SmallestDifference smallestDifference;
+
+    @Before
+    public void setup() {
+        smallestDifference = new SmallestDifference();
+    }
+
+    @Test
+    public void TestCase1() {
+        int[] expected = {28, 26};
+        assertThat(smallestDifference.smallestDifference(new int[]{-1, 5, 10, 20, 28, 3}, new int[]{26, 134, 135, 15, 17}),
+                        is(equalTo(expected)));
+    }
+
+    @Test
+    public void TestCase2() {
+        int[] expected = {28, 28};
+        assertThat(smallestDifference.smallestDifference(new int[]{-1, 1000, 28}, new int[]{28, 134, 135, 15}),
+                        is(equalTo(expected)));
+    }
+
+    @Test
+    public void TestCase3() {
+        int[] expected = {-1, -20};
+        assertThat(smallestDifference.smallestDifference(new int[]{-1}, new int[]{29, 134, 135, -20}),
+                is(equalTo(expected)));
+    }
+}
