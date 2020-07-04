0e139a9c8184b9a20d02db259921d17e53f795c6
bjonke00 <bjonke00@gmail.com>
2020-07-04 15:04:31 -0400

    add stack implementation

--- AlgorithmPractice/src/com/algorithmpractice/leetcode/BalancedBrackets.java
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/BalancedBrackets.java
@@ -1,9 +1,6 @@
 package com.algorithmpractice.leetcode;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class BalancedBrackets {
     //time O(n) space O(n)
@@ -32,5 +29,33 @@ public class BalancedBrackets {
         }
 
         return deque.peek()==null;
+    }
+
+    //stack datastructure
+    public boolean isValid1(String s) {
+        //create map of valid bracket pairs
+        //<open bracket, closing bracket>
+        Map<Character,Character> brackets = new HashMap();
+        brackets.put('(',')');
+        brackets.put('{','}');
+        brackets.put('[',']');
+
+        //create stack
+        Stack<Character> stack = new Stack();
+
+        //add characters from string to stack
+        for(int i=0; i<s.length(); i++){
+            Character bracket = s.charAt(i);
+            //if opening bracket add it to stack
+            if(brackets.containsKey(bracket)){
+                stack.push(bracket);
+            }else if (!stack.empty() && bracket == brackets.get(stack.peek())){ //stack throws emptystackexception
+                stack.pop();
+            }else{
+                return false;
+            }
+        }
+
+        return stack.empty();
     }
 }
