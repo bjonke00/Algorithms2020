30059a6cce80e34e880b6bd4dcf9e2bc1eda6615
bjonke00 <bjonke00@gmail.com>
2020-07-22 00:29:00 -0400

    update LineIsStraight solution

--- AlgorithmPractice/src/com/algorithmpractice/leetcode/easy/LineIsStraight.java
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/easy/LineIsStraight.java
@@ -2,28 +2,18 @@ package com.algorithmpractice.leetcode.easy;
 
 public class LineIsStraight {
     public boolean checkStraightLine(int[][] coordinates) {
-        int x = coordinates[1][0] - coordinates[0][0];
-        int y = coordinates[1][1]- coordinates[0][1];
+        int x1 = coordinates[0][0], x2 = coordinates[1][0];
+        int y1 = coordinates[0][1], y2 = coordinates[1][1];
+        int x = x2-x1;
+        int y = y2-y1;
 
-        for(int i=0; i<coordinates.length-1; i++){
-            if(!isLinear(coordinates[i],coordinates[i+1], x, y)){
+        for (int i = 2; i < coordinates.length; i++) {
+            int currentX = coordinates[i][0] - coordinates[i-1][0];
+            int currentY = coordinates[i][1] - coordinates[i-1][1];
+            if(y*currentX != x*currentY)
                 return false;
-            }
         }
+
         return true;
-    }
-
-    //check all the slopes are the same as the baseline slope
-    private boolean isLinear(int[] p1, int[] p2, int x, int y){
-        int x1 = p1[0], y1 = p1[1], x2 = p2[0], y2 = p2[1];
-
-        //make sure not to divide by zero
-        if(x2-x1 == 0 || x == 0){
-            if(x == x2-x1)
-                return true;
-            return false;
-        }
-
-        return (y2-y1)/(x2-x1) == y/x;
     }
 }
