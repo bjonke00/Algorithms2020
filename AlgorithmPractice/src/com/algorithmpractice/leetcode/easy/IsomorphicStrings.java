e1a3177dd89a50edd168314133d9117c38662656
bjonke00 <bjonke00@gmail.com>
2020-07-23 00:10:14 -0400

    Isomorphic Strings

--- AlgorithmPractice/src/com/algorithmpractice/leetcode/easy/IsomorphicStrings.java
+++ AlgorithmPractice/src/com/algorithmpractice/leetcode/easy/IsomorphicStrings.java
@@ -1,4 +1,20 @@
 package com.algorithmpractice.leetcode.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsomorphicStrings {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character, Character> map = new HashMap();
+        for (int i = 0; i < s.length(); i++) {
+            if (!map.containsKey(s.charAt(i))) {
+                if (map.containsValue(t.charAt(i)))
+                    return false;
+                map.put(s.charAt(i), t.charAt(i));
+            } else if (map.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- AlgorithmPractice/test/com/algorithmpractice/leetcode/easy/IsomorphicStringsTest.java
+++ AlgorithmPractice/test/com/algorithmpractice/leetcode/easy/IsomorphicStringsTest.java
@@ -1,5 +1,39 @@
+package com.algorithmpractice.leetcode.easy;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class IsomorphicStringsTest {
+
+    @Test
+    public void test1(){
+        IsomorphicStrings i = new IsomorphicStrings();
+        assertFalse(i.isIsomorphic("ab","aa"));
+    }
+
+    @Test
+    public void test2(){
+        IsomorphicStrings i = new IsomorphicStrings();
+        assertFalse(i.isIsomorphic("foo","bar"));
+    }
+
+    @Test
+    public void test21(){
+        IsomorphicStrings i = new IsomorphicStrings();
+        assertFalse(i.isIsomorphic("aba","baa"));
+    }
+
+    @Test
+    public void test3(){
+        IsomorphicStrings i = new IsomorphicStrings();
+        assertTrue(i.isIsomorphic("add","egg"));
+    }
+
+    @Test
+    public void test4(){
+        IsomorphicStrings i = new IsomorphicStrings();
+        assertTrue(i.isIsomorphic("paper","title"));
+    }
 
 }
