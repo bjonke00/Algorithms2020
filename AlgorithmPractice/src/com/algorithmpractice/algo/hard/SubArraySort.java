cbb1a482a4533e312552119012bb8c575ae929bd
bjonke00 <bjonke00@gmail.com>
2020-09-21 22:02:36 -0400

    SubArray Sort

---
+++ AlgorithmPractice/src/com/algorithmpractice/algo/hard/SubArraySort.java
@@ -0,0 +1,47 @@
+package com.algorithmpractice.algo.hard;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SubArraySort {
+
+    public static int[] subarraySort(int[] array) {
+        int minUnsorted = Integer.MAX_VALUE;
+        int maxUnsorted = Integer.MIN_VALUE;
+
+        for(int i=0; i<array.length; i++){
+            if(isUnsorted(i, array)){
+                minUnsorted = Math.min(minUnsorted, array[i]);
+                maxUnsorted = Math.max(maxUnsorted, array[i]);
+            }
+        }
+
+        if(minUnsorted == Integer.MAX_VALUE){
+            return new int[]{-1,-1};
+        }
+
+        int start = 0;
+        while(array[start] <= minUnsorted){
+            start++;
+        }
+
+        int end = array.length - 1;
+        while(array[end] >= maxUnsorted){
+            end--;
+        }
+
+        return new int[]{start, end};
+    }
+
+    private static boolean isUnsorted(int i, int[] array){
+        if(i == 0){
+            return array[i] > array[i+1];
+        }
+        if(i == array.length-1){
+            return array[i-1] > array[i];
+        }
+        return array[i-1] > array[i] || array[i] > array[i+1];
+    }
+
+
+}
---
+++ AlgorithmPractice/test/com/algorithmpractice/algo/binarytree/hard/SubArraySortTest.java
@@ -0,0 +1,30 @@
+package com.algorithmpractice.algo.binarytree.hard;
+
+import com.algorithmpractice.algo.hard.SubArraySort;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class SubArraySortTest {
+
+    @Test
+    public void TestCase_unsorted() {
+        int[] expected = {3, 9};
+        assertTrue(
+                Arrays.equals(
+                        SubArraySort.subarraySort(new int[] {1, 2, 4, 7, 10, 11, 7, 12, 6, 7, 16, 18, 19}),
+                        expected));
+    }
+
+    @Test
+    public void TestCase_sorted() {
+        int[] expected = {-1, -1};
+        assertTrue(
+                Arrays.equals(
+                        SubArraySort.subarraySort(new int[] {1, 2, 4, 7, 10, 11, 12, 16, 18, 19}),
+                        expected));
+    }
+
+}
