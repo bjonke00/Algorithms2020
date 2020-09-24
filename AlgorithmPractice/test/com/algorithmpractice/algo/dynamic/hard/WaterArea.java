bbdae01af94c025fefa72c403425896e8cfc9988
bjonke00 <bjonke00@gmail.com>
2020-09-23 23:57:49 -0400

    WaterArea

--- AlgorithmPractice/test/com/algorithmpractice/algo/dynamic/hard/WaterArea.java
+++ AlgorithmPractice/test/com/algorithmpractice/algo/dynamic/hard/WaterArea.java
@@ -1,4 +1,35 @@
 package com.algorithmpractice.algo.dynamic.hard;
 
+import java.util.stream.IntStream;
+
 public class WaterArea {
+    //time O(n) | space O(n)
+    public static int waterArea(int[] heights) {
+        if(heights.length < 1)
+            return 0;
+
+        int[] right = new int[heights.length];
+        right[0] = heights[0];
+        for(int i=1; i<heights.length; i++){
+            right[i] = Math.max(right[i-1], heights[i]);
+        }
+
+        int[] left = new int[heights.length];
+        left[heights.length-1] = heights[heights.length-1];
+        for(int i=heights.length-2; i>=0; i--){
+            left[i] = Math.max(left[i+1], heights[i]);
+        }
+
+        int[] min = new int[heights.length];
+        for(int i=0; i<min.length; i++){
+            min[i] = Math.min(right[i], left[i]);
+        }
+
+        int[] waterArea = new int[heights.length];
+        for(int i=0; i<waterArea.length; i++){
+            waterArea[i] = min[i] - heights[i];
+        }
+
+        return IntStream.of(waterArea).sum();
+    }
 }
--- AlgorithmPractice/test/com/algorithmpractice/algo/dynamic/hard/WaterAreaTest.java
+++ AlgorithmPractice/test/com/algorithmpractice/algo/dynamic/hard/WaterAreaTest.java
@@ -1,5 +1,14 @@
+package com.algorithmpractice.algo.dynamic.hard;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class WaterAreaTest {
+    @Test
+    public void TestCase1() {
+        int[] input = {0, 8, 0, 0, 5, 0, 0, 10, 0, 0, 1, 1, 0, 3};
+        assertTrue(WaterArea.waterArea(input) == 48);
+    }
 
 }
