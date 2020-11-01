f56860a7e8120ee3c61cf539810e57905fff3b0d
bjonke00 <bjonke00@gmail.com>
2020-10-31 22:33:39 -0400

    update group anagrams

--- AlgorithmPractice/src/com/algorithmpractice/algo/medium/GroupAnagrams.java
+++ AlgorithmPractice/src/com/algorithmpractice/algo/medium/GroupAnagrams.java
@@ -8,25 +8,22 @@ public class GroupAnagrams {
     Anagrams are strings made up of exactly the same letters, where order does NOT matter.
      */
 
-    //Time O(n) & space O(n)
+    //Time O(n*m*log(n)) & space O(n*m) // where n is the number of words and m is the longest word
     public static List<List<String>> groupAnagrams(List<String> words) {
         //map of anagrams --key is the ordered anagram and value is list of original values
-        Map<String, List<String>> anagrams = new HashMap<>();
+        Map<String, List<String>> anagramGroups = new HashMap<>();
 
         //sort the words & add to map
         for (String word : words) {
-            char[] sortedChars = word.toCharArray();
-            Arrays.sort(sortedChars);
-            String sortedWord = new String(sortedChars);
-            List<String> temp = anagrams.get(sortedWord);
-            if (temp == null) {
-                temp = new ArrayList<>();
-            }
-            temp.add(word);
-            anagrams.put(sortedWord, temp);
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String sortedWord = String.valueOf(chars);
+            List<String> anagramGroup = anagramGroups.getOrDefault(sortedWord, new ArrayList<String>());
+            anagramGroup.add(word);
+            anagramGroups.put(sortedWord, anagramGroup);
         }
 
-        return new ArrayList(anagrams.values());
+        return new ArrayList(anagramGroups.values());
     }
 
 }
