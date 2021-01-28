0c1dc8cb511c7a81b2d87057da921fb6a370d2d0
bjonke00 <bjonke00@gmail.com>
2021-01-27 21:28:28 -0500

    enums

---
+++ AlgorithmPractice/src/com/algorithmpractice/javapractice/EnumsPractice.java
@@ -0,0 +1,32 @@
+package com.algorithmpractice.javapractice;
+
+public class EnumsPractice {
+
+    enum Days{
+        MONDAY, TUESDAY, WEDNESDAY
+    }
+
+    enum Transportation{
+        CAR(120), BICYCLE(30), SKATE_BOARD(15);
+        int maxSpeed;
+
+        Transportation(int maxSpeed){
+            this.maxSpeed = maxSpeed;
+        }
+    }
+    public static void main(String[] args) {
+        Days monday = Days.MONDAY;
+
+        for(Days day : Days.values()){
+            System.out.println(day);
+        }
+
+        int currentMaxSpeed = Transportation.BICYCLE.maxSpeed;
+        System.out.println(currentMaxSpeed);
+
+        for(Transportation transportation : Transportation.values()){
+            System.out.println(String.format("%s %s %s",transportation.ordinal(), transportation.name(), transportation.maxSpeed));
+        }
+
+    }
+}
